83e33ffd20375513f0dd4188ae8c88de8c247ca5
Jime <jimedanieli.09@gmail.com>
2022-03-02 00:03:50 -0300

    Actions

---
+++ blog-app/src/actions/index.js
@@ -0,0 +1,12 @@
+export function getAllUsers(){
+    return {type: 'GET_ALL_USERS', payload}
+}
+export function getAllUsersPosts(){
+    return {type: 'GET_ALL_USERS', payload}
+}
+export function getAllCommentsPost(){
+    return {type: 'GET_ALL_USERS', payload}
+}
+export function getAllPosts(){
+    return {type: 'GET_ALL_USERS', payload}
+}
