93674498b51cbc201c46ea530ad20f94c7c99de5
Jime <jimedanieli.09@gmail.com>
2022-03-02 16:55:13 -0300

    Reducer ready

--- blog-app/src/actions/index.js
+++ blog-app/src/actions/index.js
@@ -1,5 +1,5 @@
 export const GET_ALL_USERS= "GET_ALL_USERS";
-export const GET_ALL_USERS_POSTS= "GET_ALL_USERS_POSTS";
+export const GET_ALL_USER_POSTS= "GET_ALL_USERS_POSTS";
 export const GET_ALL_COMMENTS_POSTS= "GET_ALL_COMMENTS_POSTS";
 export const GET_ALL_POSTS= "GET_ALL_POSTS";
 
@@ -14,7 +14,7 @@ export function getAllUsersPosts(id){
     return function(dispatch){
         return fetch(`https://jsonplaceholder.typicode.com/posts?userId=${id}`)
         .then(response => response.json())
-        .then(json => dispatch({type: GET_ALL_USERS_POSTS, payload: json}))
+        .then(json => dispatch({type: GET_ALL_USER_POSTS, payload: json}))
     }
 }
 
---
+++ blog-app/src/reducers/index.js
@@ -0,0 +1,44 @@
+import {
+    GET_ALL_USERS,
+    GET_ALL_COMMENTS_POSTS,
+    GET_ALL_POSTS,
+    GET_ALL_USER_POSTS
+} from '../actions/index'
+//me traigo los tipos en vez de las actions en si, para evitar errores de tipeo. De todas formas la info ya esta guradada
+
+const initialState={
+    users: [],
+    posts: [],
+    usersPosts: [],
+    commentsPosts: []
+};
+
+export default function rootReducer(state = initialState, action){
+    switch(action.type){
+        case GET_ALL_USERS:
+            return {
+                ...state,
+                users: action.payload,//esto me trae el payload desde actions, o sea json o data
+            };
+        case GET_ALL_POSTS:
+            return {
+                ...state,
+                posts: action.payload,
+            };
+       case GET_ALL_USER_POSTS:
+           return { 
+                ...state,
+                usersPosts: action.payload// de esta forma si se pisa la info, abajo no
+                //userPosts: state.userPost.concat(action.payload)
+                //userPosts: [...state.userPosts, ...action.payload]
+                //[{post}, {post}]
+           };
+        case GET_ALL_COMMENTS_POSTS:
+            return {
+                ...state,
+                commentsPosts: action.payload
+            };
+        default:
+            return state;
+    }
+}
