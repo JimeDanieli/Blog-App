18a5c921cfaa8450504cc8faeb4eaea7efe76c8b
Jime <jimedanieli.09@gmail.com>
2022-03-08 21:23:51 -0300

    Store almost done

--- blog-app/src/store/index.js
+++ blog-app/src/store/index.js
@@ -1,3 +1,12 @@
 import {createStore, applyMiddleware, compose} from 'redux'
 import reducer from '../reducer/index';
-import thunk from 'redux-thunk'
+import thunk from 'redux-thunk' //me sirve para usar acciones asincronicas
+
+const composeEnhancers= window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
+export const store=createStore(
+    reducer,
+    composeEnhancers(
+        applyMiddleware(thunk)
+    )
+)
